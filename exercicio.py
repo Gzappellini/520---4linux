52109123e82c9dcf12fd9a4d644c4656b7a5c6ea
leonardo.mendes@4linux.com.br <leonardo.mendes@4linux.com.br>
2019-09-14 13:34:46 -0300

    aula2

--- exercicio.py
+++ exercicio.py
@@ -2,19 +2,22 @@
 with open('teste.txt', 'r') as arq:
     conteudo = arq.readlines()
 
-
+print(conteudo)
 #altera conteudo numerando itens
 cont = 0
 aux = []
 for x in conteudo:
     aux.append('{}- {}'.format(cont, x)) 
+    cont += 1
+print(aux)
+
 
 #altera conteudo numerando itens
 #conteudo = [f'{c+1}- {x}' for c, x in enumerate(conteudo)]
 
-#reescreve contteudo antigo
+#reescreve conteudo antigo
 
 with open ('teste.txt', 'w') as arq:
-    arq.writelines(conteudo)
+    arq.writelines(aux)
 
 
